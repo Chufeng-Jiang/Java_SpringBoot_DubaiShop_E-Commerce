d507e76afa8c5be04fafcc307a2e8b1ac1a2406c
cxj164 <cxj164@cs.bham.ac.uk>
2022-07-09 19:00:15 +0400

    Add Backend User Folder

---
+++ 04-Project Source Code/DuShopProject/DuShopCommon/src/main/java/com/dushop/common/entity/IdBasedEntity.java
@@ -0,0 +1,4 @@
+package com.dushop.common.entity;
+
+public class IdBasedEntity {
+}
---
+++ 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/user/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.dushop.admin.user;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface RoleRepository extends  CrudRpository<Role, Event.ID>{
+}
