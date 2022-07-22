a72d0369f10a764ad0922a2fc826cb4e50d1c153
cxj164 <cxj164@cs.bham.ac.uk>
2022-07-22 18:22:54 +0400

    fixed CSV export bug

--- 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/user/UserCsvExporter.java
+++ 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/user/UserCsvExporter.java
@@ -22,10 +22,26 @@ import org.supercsv.prefs.CsvPreference;
 
 public class UserCsvExporter extends AbstractExporter {
 
+   /*
+    https://codehunter.cc/a/spring/how-to-return-csv-data-in-browser-from-spring-controller
+    https://grabthiscode.com/whatever/download-csv-file-spring-boot
+    */
+
     public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
         super.setResponseHeader(response, "text/csv", ".csv");
 
         ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(),
                 CsvPreference.STANDARD_PREFERENCE);
+
+        String[] csvHeader = {"User ID", "E-mail", "First Name", "Last Name", "Roles", "Enabled"};
+        String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (User user : listUsers) {
+            csvWriter.write(user, fieldMapping);
+        }
+
+        csvWriter.close();
     }
 }
