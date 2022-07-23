25b582f94c47cc52a49d3b48b5c10c0c62773094
cxj164 <cxj164@cs.bham.ac.uk>
2022-07-23 19:28:40 +0400

    User Details Update

--- 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/resources/static/js/common.js
+++ 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/resources/static/js/common.js
@@ -21,4 +21,22 @@ $(document).ready(function() {
         e.preventDefault();
         document.logoutForm.submit();
     });
+
+    editDropdown_menu();
 });
+
+function editDropdown_menu() {
+    $(".navbar .dropdown").hover(
+        function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+        },
+        function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+        }
+    );
+
+    $(".dropdown > a").click(function() {
+        location.href = this.href;
+    });
+
+}
