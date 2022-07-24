497313acbfc61b55b2bdd439bd3bb19bf1baae05
cxj164 <cxj164@cs.bham.ac.uk>
2022-07-24 16:09:23 +0400

    Fixed bug of hang up in hibernate, don't lock the table when witing data

--- 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/category/CategoryService.java
+++ 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/category/CategoryService.java
@@ -1,6 +1,8 @@
 package com.dushop.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +26,52 @@ public class CategoryService {
     public List<Category> listAll() {
         return (List<Category>) repo.findAll();
     }
+
+    /*
+     * @description: Resuse from test
+     * @author: Jiang Chufeng
+     * @date: 2022/7/24 15:01
+     * @param:
+     * @return: java.util.List<com.dushop.common.entity.Category>
+     */
+    public List<Category> listCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+
+        Iterable<Category> categoriesInDB = repo.findAll();
+
+        for (Category category : categoriesInDB) {
+            if (category.getParent() == null) {
+                categoriesUsedInForm.add(new Category(category.getName()));
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    String name = "--" + subCategory.getName();
+                    categoriesUsedInForm.add(new Category(name));
+
+                    listChildren(categoriesUsedInForm, subCategory, 1);
+                }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for (Category subCategory : children) {
+            String name = "";
+            for (int i = 0; i < newSubLevel; i++) {
+                name += "--";
+            }
+            name += subCategory.getName();
+
+            categoriesUsedInForm.add(new Category(name));
+
+            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+        }
+    }
+
 }
