92090c7471c5a37f0793ae53cb7aae3c69799f05
cxj164 <cxj164@cs.bham.ac.uk>
2022-07-24 16:50:02 +0400

    Subcategories

--- 04-Project Source Code/DuShopProject/DuShopCommon/src/main/java/com/dushop/common/entity/Category.java
+++ 04-Project Source Code/DuShopProject/DuShopCommon/src/main/java/com/dushop/common/entity/Category.java
@@ -55,6 +55,23 @@ public class Category {
         this.id = id;
     }
 
+    public static Category copyIdAndName(Category category) {
+        //set the category to the subcategory set -->CategoryService--listCategoriesUsedInForm
+        Category copyCategory = new Category();
+        copyCategory.setId(category.getId());
+        copyCategory.setName(category.getName());
+
+        return copyCategory;
+    }
+
+    public static Category copyIdAndName(Integer id, String name) {
+        Category copyCategory = new Category();
+        copyCategory.setId(id);
+        copyCategory.setName(name);
+
+        return copyCategory;
+    }
+
     public Category(String name) {
         this.name = name;
         this.alias = name;
