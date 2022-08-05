7fc8580c035f19e8c20f4ce137dc16eb63e85830
cxj164 <cxj164@cs.bham.ac.uk>
2022-08-05 13:32:23 +0400

    brand pagination

--- 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/brand/BrandRepository.java
+++ 04-Project Source Code/DuShopProject/DuShopWebParent/DuShopBackEnd/src/main/java/com/dushop/admin/brand/BrandRepository.java
@@ -1,6 +1,9 @@
 package com.dushop.admin.brand;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dushop.common.entity.Brand;
 
@@ -8,4 +11,7 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
     public Long countById(Integer id);
 
     public Brand findByName(String name);
+
+    @Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+    public Page<Brand> findAll(String keyword, Pageable pageable);
 }
